e4c174f8a1246b2cab04ac0a433e547587bddc1c
rizalnramadhan <rizalnramadhan@gmail.com>
2020-07-10 16:45:41 +0700

    Initial repository

---
+++ assets/audio.js
@@ -0,0 +1,43 @@
+$("#hide").click(function(){
+  $("#hide").hide();
+});
+
+var welcome = $("#audio-welcome")[0];
+$("#welcome").mouseover(function() {
+  welcome.play();
+});
+$("#welcome").mouseout(function() {
+  welcome.load();
+});
+
+var narration = $("#audio-narration")[0];
+$("#narration").mouseover(function() {
+  narration.play();
+});
+$("#narration").mouseout(function() {
+  narration.load();
+});
+
+var listen = $("#audio-listen")[0];
+$("#listen").mouseover(function() {
+  listen.play();
+});
+$("#listen").mouseout(function() {
+  listen.load();
+});
+
+var ask = $("#audio-ask")[0];
+$("#ask").mouseover(function() {
+  ask.play();
+});
+$("#ask").mouseout(function() {
+  ask.load();
+});
+
+var about = $("#audio-about")[0];
+$("#about").mouseover(function() {
+  about.play();
+});
+$("#about").mouseout(function() {
+  about.load();
+});
